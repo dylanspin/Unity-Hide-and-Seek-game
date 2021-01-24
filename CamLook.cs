62fcdac0002b50ea371ad5fedfb6a79fc947a8ca
Dylan Spin <dylanspin100@hotmail.com>
2021-01-24 21:15:58 +0100

    Basics

---
+++ CamLook.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamLook : MonoBehaviour
+{
+    public float sensitivity = 1.5f;
+    public Transform playerBody;
+    float xRotation = 0.0f;
+    float yRotation = 0.0f;
+    public bool ismine = true; 
+    public float minAngle = -65;
+    public float maxAngel = 90;
+
+
+    void Start()
+    {
+        // // Debug.Log(save.loadOptions().Sens+" som : " + save.loadOptions().Sens / 100);
+        // if(save.loadOptions() != null)
+        // {
+        //     if((float)save.loadOptions().Sens <= 0.1f)
+        //     {
+        //         sensitivity = 3f * 0.1f;
+        //     }else{
+        //         sensitivity = 3f * (float)((float)save.loadOptions().Sens  / 100);
+        //     }
+        // }else{
+        //     sensitivity = 1.5f;
+        // }
+    }
+    
+    void Update ()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * sensitivity;
+        float mouseY = Input.GetAxis("Mouse Y") * sensitivity;
+        
+        yRotation -= mouseX;
+        yRotation = Mathf.Clamp(yRotation, -60, 60);
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, minAngle, maxAngel);
+
+        
+        transform.localRotation = Quaternion.Euler(xRotation,-yRotation,0f); 
+                
+    }
+}
---
+++ Movement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public CharacterController controller;
+    public Transform grounCheck;
+    public float GroundDistance = 0.4f;
+    public LayerMask groundMask;
+    public bool isGrounded = false;
+
+    public bool running = false;
+    private bool crouching = false;
+
+    public float WalkingSpeed = 12.0f;
+    public float RuningSpeed = 14.0f;
+    public float crouchingSpeed = 7.0f;
+    public float jumpHeight = 3.0f;
+    public float gravity = -9.81f;
+    public float speed = 15.0f;
+    private Vector3 velocity;
+
+    void Update()
+    {
+        // if(ismine)
+        // {
+            isGrounded = controller.isGrounded;
+            if(isGrounded && velocity.y < 0)
+            {
+                velocity.y = -2f;   
+            }
+
+            float z = Input.GetAxis("Vertical");
+            float x = Input.GetAxis("Horizontal");
+
+            
+            Vector3 move = transform.right * x + transform.forward * z;
+
+            controller.Move(move * speed * Time.deltaTime);//movement
+
+            velocity.y += gravity * Time.deltaTime;
+
+            controller.Move(velocity * Time.deltaTime);//gravity
+        // }
+    }
+}
